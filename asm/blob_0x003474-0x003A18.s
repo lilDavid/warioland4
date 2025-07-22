878cf747956c018abea482132706c69881310f49
lil David <1337lilDavid@gmail.com>
2025-07-22 06:34:07 -0500

    Remove unused code blobs

--- asm/blob_0x003474-0x003A18.s
+++
@@ -1,7 +0,0 @@
-.include "macros.s.inc"
-
-
-thumb_func_start func_8003474
-func_8003474:
-
-baserom_blob 0x003474, 0x003A18
--- asm/blob_0x003F88-0x00FD28.s
+++
@@ -1,4 +0,0 @@
-.include "macros.s.inc"
-
-
-baserom_blob 0x0046D8, 0x00FD28
