a1924d64dbc2caff0a3975dbf5c149dc44e266d6
TsilaAllaoui <tsila.allaoui@gmail.com>
2026-06-29 14:24:02 +0300

    Better return flow

--- src/cutscenes/c_0x00F7BC.c
+++ src/cutscenes/c_0x00F7BC.c
@@ -4,75 +4,73 @@ s32 func_800F7BC(s32 arg0, u8 ** arg1)
 {
     if (arg0 <= 7) {
         *arg1 = &sUnk_82D270C;
-        goto done;
+        return arg0 > 0x12A;
     }
     if (arg0 <= 15) {
         *arg1 = &sUnk_82D2714;
-        goto done;
+        return arg0 > 0x12A;
     }
     if (arg0 <= 23) {
         *arg1 = &sUnk_82D2722;
-        goto done;
+        return arg0 > 0x12A;
     }
     if (arg0 <= 31) {
         *arg1 = &sUnk_82D272A;
-        goto done;
+        return arg0 > 0x12A;
     }
     if (arg0 <= 47) {
         *arg1 = &sUnk_82D2738;
-        goto done;
+        return arg0 > 0x12A;
     }
     if (arg0 <= 55) {
         *arg1 = &sUnk_82D2746;
-        goto done;
+        return arg0 > 0x12A;
     }
     if (arg0 <= 63) {
         *arg1 = &sUnk_82D275A;
-        goto done;
+        return arg0 > 0x12A;
     }
     if (arg0 <= 71) {
         *arg1 = &sUnk_82D276E;
-        goto done;
+        return arg0 > 0x12A;
     }
     if (arg0 <= 87) {
         *arg1 = &sUnk_82D2956;
-        goto done;
+        return arg0 > 0x12A;
     }
     if (arg0 <= 95) {
         *arg1 = &sUnk_82D2788;
-        goto done;
+        return arg0 > 0x12A;
     }
     if (arg0 <= 103) {
         *arg1 = &sUnk_82D27B4;
-        goto done;
+        return arg0 > 0x12A;
     }
     if (arg0 <= 111) {
         *arg1 = &sUnk_82D27E0;
-        goto done;
+        return arg0 > 0x12A;
     }
     if (arg0 <= 119) {
         *arg1 = &sUnk_82D2812;
-        goto done;
+        return arg0 > 0x12A;
     }
     if (arg0 <= 139) {
         *arg1 = &sUnk_82D283E;
-        goto done;
+        return arg0 > 0x12A;
     }
     if (arg0 <= 159) {
         *arg1 = &sUnk_82D286A;
-        goto done;
+        return arg0 > 0x12A;
     }
     if (arg0 <= 179) {
         *arg1 = &sUnk_82D289C;
-        goto done;
+        return arg0 > 0x12A;
     }
     if (arg0 <= 199) {
         *arg1 = &sUnk_82D28D4;
-        goto done;
+        return arg0 > 0x12A;
     }
     *arg1 = &sUnk_82D2912;
-
-done:
     return arg0 > 0x12A;
 }
 
@@ -80,51 +78,49 @@ s32 func_800F8A4(s32 arg0, u8 ** arg1)
 {
     if (arg0 <= 7) {
         *arg1 = &sUnk_82D2976;
-        goto done;
+        return arg0 > 0xFC;
     }
     if (arg0 <= 15) {
         *arg1 = &sUnk_82D297E;
-        goto done;
+        return arg0 > 0xFC;
     }
     if (arg0 <= 23) {
         *arg1 = &sUnk_82D298C;
-        goto done;
+        return arg0 > 0xFC;
     }
     if (arg0 <= 31) {
         *arg1 = &sUnk_82D2994;
-        goto done;
+        return arg0 > 0xFC;
     }
     if (arg0 <= 47) {
         *arg1 = &sUnk_82D29A2;
-        goto done;
+        return arg0 > 0xFC;
     }
     if (arg0 <= 55) {
         *arg1 = &sUnk_82D29B0;
-        goto done;
+        return arg0 > 0xFC;
     }
     if (arg0 <= 63) {
         *arg1 = &sUnk_82D29BE;
-        goto done;
+        return arg0 > 0xFC;
     }
     if (arg0 <= 93) {
         *arg1 = &sUnk_82D29D2;
-        goto done;
+        return arg0 > 0xFC;
     }
     if (arg0 <= 113) {
         *arg1 = &sUnk_82D29E6;
-        goto done;
+        return arg0 > 0xFC;
     }
     if (arg0 <= 133) {
         *arg1 = &sUnk_82D2A00;
-        goto done;
+        return arg0 > 0xFC;
     }
     if (arg0 <= 153) {
         *arg1 = &sUnk_82D2A20;
-        goto done;
+        return arg0 > 0xFC;
     }
     *arg1 = &sUnk_82D2A46;
-
-done:
     return arg0 > 0xFC;
 }
 
